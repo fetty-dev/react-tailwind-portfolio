9ee9edd9aa438277bafe7dfba8ae7896c9a690e8
fetty <jxfetdominguez11@pm.me>
2025-07-02 09:30:40 -0500

    Update footer copyright to reflect correct company name

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export const Footer = () => {
       {" "}
       <p className="text-sm text-muted-foreground">
         {" "}
-        &copy; {new Date().getFullYear()} JxfetTch.co. All rights reserved.
+        &copy; {new Date().getFullYear()} Fetty.dev.co. All rights reserved.
       </p>
       <a
         href="#hero"
